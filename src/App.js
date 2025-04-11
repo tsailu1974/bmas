bfed61cb52e88a3882f1d81468c29770c3282c26
Sailaja <tsailu1974@gmail.com>
2025-04-11 15:18:40 -0500

    Completed Design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,68 @@
 import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const NavigationTabs = () => {
+  const [activeTab, setActiveTab] = useState("Home");
+
   return (
     <div className="App">
-      <h1>BMAS Application</h1>
-      <p>This application is used by insurance providers & brokers.</p>
+      <div className='navbar'>
+      {["Home", "Group", "Employee"].map((tab) => (
+          <span
+            key={tab}
+            className={`nav-item ${activeTab === tab ? "active" : ""}`}
+            onClick={() => setActiveTab(tab)}
+          >
+            {tab}
+          </span>
+        ))}
+      </div>
+      <hr className="underline" />
+       {/* Group Subtabs */}
+       {activeTab === "Group" && (
+        <>
+          <div className="subtabs">
+            <span className="sub-tab">New Group</span>
+            <span className="sub-tab">Search Group</span>
+          </div>
+          <hr className="sub-underline" />
+        </>
+      )}
+      {/* Employee Subtabs */}
+      {activeTab === "Employee" && (
+        <>
+          <div className="subtabs">
+            <span className="sub-tab">New Employee</span>
+            <span className="sub-tab">Edit Employee</span>
+          </div>
+          <hr className="sub-underline" />
+        </>
+      )}
+      <div className='search-form-wrapper'>
+      <h3 className='sectionTitle'>Group Search</h3>
+      <hr className='hr'/>
+          <form>
+            <div className='form-row'>
+              <div className='input-group'>
+                <label className='label'>Group Name</label>
+                <input className='input' type="text" name="groupName" />
+              </div>
+              <div className='input-group'>
+                <label className='label'>Group Id</label>
+                <input  className='input'type="text" name="groupId" />
+              </div>
+            </div>
+            <div className='search-button-wrapper'>
+              <button className='search-button' type="submit">Search</button> 
+            </div>
+          </form>
+        </div>
     </div>
   );
+};
+
+function App() {
+  return <NavigationTabs />;
 }
 
 export default App;
